c17424bc67b789c773a5a738f7972137fc67507d
mlnyusufm <165493829+mlnyusufm@users.noreply.github.com>
2024-03-30 23:27:51 +0700

    kelas kedua
    menambahkan class character 
    MAULANA YUSUF MUHAMMAD (21120123140166)

--- DragonCave.py
+++ DragonCave.py
@@ -35,6 +35,41 @@ class Character:
         self.potion = potion
 
 #class keduanya
+class DragonCaveGame:
+    def _init_(self):
+        self.naga_baik = random.randint(1, 2)
+        self.naga_jahat = 2 if self.naga_baik == 1 else 1
+        self.kesempatan_kabur = random.randint(1, 50)
+
+        self.player = Character(200, 4)
+        self.naga = Character(2000, 0)
+
+    def play(self):
+        print("Kamu melihat dua gua di depanmu ...")
+        time.sleep(1)
+        gua = int(input("Apakah kamu memilih gua 1 atau 2?"))
+
+        if gua == self.naga_baik:
+            print("Gua ini gelap dan menakutkan…")
+            time.sleep(1)
+            print("Seekor naga besar muncul di depanmu! Ia membuka mulutnya dan...")
+            time.sleep(1)
+            print("Isi mulutnya berisi harta yang ingin dibagikan kepadamu, selamat kamu menemukan naga yang ramah")
+
+        elif gua == self.naga_jahat:
+            print("Naga itu mengeluarkan api dari mulutnya, dan kau hampir saja kena")
+            time.sleep(1)
+            decision = input("Apakah kau akan 'kabur' atau 'melawan' naga itu?")
+
+            if decision == "kabur":
+                if self.kesempatan_kabur >= 24:
+                    print("Kamu berhasil kabur dari sergapan naga jahat itu")
+                else:
+                    print("Naga itu berhasil menangkapmu dan kau dibunuh")
+                    return
+            elif decision == "melawan":
+                print("Sepertinya pedang yang kamu bawa berguna juga")
+                time.sleep(1)
 #Perulangan while
 
 #fase naganya 
